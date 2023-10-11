8948009223502d7fc91701b3990314e2baf92ced
Michal <michalpetrycki@gmail.com>
2023-10-11 13:33:26 +0100

    Extend controllers with route params

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace beginner.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id = 3, Name="Sam"}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+global using beginner.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
